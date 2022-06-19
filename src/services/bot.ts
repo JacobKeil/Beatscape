0bfe1b5af416a9e121e0bd52eba641a6736552fd
Jacob <jacobkeil13@gmail.com>
2022-06-19 03:40:40 -0400

    simplified entry point into the extended class

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -1,28 +1,7 @@
-import { Client, Intents } from 'discord.js';
 import { config } from 'dotenv';
 config();
 
-import { BeatscapeClient } from '../common/interfaces';
-import { registerCommands } from '../utils/commands.js';
+import { CustomClient } from '../common/interfaces.js';
 
-const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
-});
-
-let Beatscape: BeatscapeClient = {
-  client: client,
-  queue: new Map(),
-};
-
-Beatscape.client.on('ready', () => {
-  console.log('Discord bot ready');
-  registerCommands(Beatscape);
-});
-
-Beatscape.client.login(process.env.BOT_TOKEN);
+const Beatscape = new CustomClient();
+Beatscape.login(process.env.BOT_TOKEN);
