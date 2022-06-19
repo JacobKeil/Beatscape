e7838e45f046e8d91f315e06de5eb1a72b856e9d
Jacob <jacobkeil13@gmail.com>
2022-06-19 03:38:34 -0400

    class now extends client with attached queue

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,12 +1,29 @@
+import { registerCommands } from '../utils/commands.js';
 import { AudioPlayer, VoiceConnection } from '@discordjs/voice';
-import { Client, VoiceBasedChannel, TextBasedChannel } from 'discord.js';
+import {
+  Client,
+  Intents,
+  VoiceBasedChannel,
+  TextBasedChannel,
+} from 'discord.js';
 
+// DiscordJS client intents
+const intents = [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_VOICE_STATES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+];
+
+/** Song object from YouTube */
 export interface Song {
   title: string;
   thumbnail: string;
   url: string;
 }
 
+/** Log object for each song request */
 export interface Log {
   discordId: string;
   guildId: string;
@@ -16,6 +33,7 @@ export interface Log {
   date: string;
 }
 
+/** Queue object for each guild */
 export interface QueuePromise {
   textChannel: TextBasedChannel;
   voiceChannel: VoiceBasedChannel;
@@ -25,7 +43,15 @@ export interface QueuePromise {
   isPaused: Boolean;
 }
 
-export interface BeatscapeClient {
-  client: Client;
+/** Main client with attached queue map */
+export class CustomClient extends Client {
   queue: Map<string, QueuePromise>;
+  constructor() {
+    super({ intents });
+    this.queue = new Map<string, QueuePromise>();
+    this.once('ready', () => {
+      console.log('Discord bot ready');
+      registerCommands(this);
+    });
+  }
 }
