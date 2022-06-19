c8432a77ebeaf99a9bf524eacb0401616c35db0b
Jacob <jacobkeil13@gmail.com>
2022-06-19 03:41:15 -0400

    replaced if statements with switch statement

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,12 +1,11 @@
-import { Guild, Constants } from 'discord.js';
-import { BeatscapeClient, QueuePromise } from '../common/interfaces';
-import { playSong } from '../interactions/play.js';
-// import { guildId } from './config.js';
+import { Constants } from 'discord.js';
+import { CustomClient } from '../common/interfaces.js';
+import { endSession } from '../utils/helpers.js';
+import controls from '../interactions/index.js';
 
-export function registerCommands(Beatscape: BeatscapeClient) {
-  // const guild: Guild = Beatscape.client.guilds.cache.get(guildId);
+export function registerCommands(Beatscape: CustomClient) {
   let commands: any;
-  commands = Beatscape.client.application.commands;
+  commands = Beatscape.application.commands;
 
   commands?.create({
     name: 'play',
@@ -41,11 +40,11 @@ export function registerCommands(Beatscape: BeatscapeClient) {
     description: 'resumes current audio transmission.',
   });
 
-  Beatscape.client.on('interactionCreate', async (interaction) => {
+  Beatscape.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
     await interaction.deferReply();
 
-    const guild = Beatscape.client.guilds.cache.get(interaction.guildId);
+    const guild = Beatscape.guilds.cache.get(interaction.guildId);
     const member = guild.members.cache.get(interaction.member.user.id);
     const voiceChannel = member.voice.channel;
 
@@ -60,61 +59,26 @@ export function registerCommands(Beatscape: BeatscapeClient) {
     const { commandName, options } = interaction;
     const search = options.getString('search');
 
-    if (commandName === 'play') {
-      if (search) {
-        try {
-          await interaction.deleteReply();
-          playSong(Beatscape, interaction, search);
-          return;
-        } catch (err) {
-          console.log(err);
-        }
+    try {
+      switch (commandName) {
+        case 'play':
+          controls.play(Beatscape, interaction, search);
+          break;
+        case 'stop':
+          controls.stop(Beatscape, interaction);
+          break;
+        case 'skip':
+          controls.skip(Beatscape, interaction);
+          break;
+        case 'pause':
+          controls.pause(Beatscape, interaction);
+          break;
+        case 'resume':
+          controls.resume(Beatscape, interaction);
+          break;
       }
-    }
-
-    const musicQueue: QueuePromise = Beatscape.queue.get(interaction.guild.id);
-
-    function onDestroy(err: any) {
-      console.log(err);
-      musicQueue.connection.destroy();
-      Beatscape.queue.delete(interaction.guild.id);
-    }
-
-    if (commandName == 'skip') {
-      try {
-        await interaction.deleteReply();
-        musicQueue.player.stop();
-      } catch (err) {
-        onDestroy(err);
-      }
-    }
-
-    if (commandName === 'stop') {
-      try {
-        await interaction.deleteReply();
-        musicQueue.connection.destroy();
-        Beatscape.queue.delete(interaction.guild.id);
-      } catch (err) {
-        onDestroy(err);
-      }
-    }
-
-    if (commandName === 'pause') {
-      try {
-        musicQueue.player.pause();
-        interaction.editReply('Song paused. Use `/resume` to start again.');
-      } catch (err) {
-        onDestroy(err);
-      }
-    }
-
-    if (commandName === 'resume') {
-      try {
-        musicQueue.player.unpause();
-        interaction.editReply('Song resumed. Use `/pause` to pause again.');
-      } catch (err) {
-        onDestroy(err);
-      }
+    } catch (err) {
+      endSession(Beatscape, interaction.guild.id, err);
     }
   });
 }
